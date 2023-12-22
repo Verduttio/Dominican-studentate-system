228b7811bb1c3c549f57a3f0a5a9d26383c3c4f4
Verduttio <bszwaja20@gmail.com>
2023-12-22 21:32:42 +0100

    Initial commit

---
+++ backend/dominicanAppBackend/src/main/java/org/verduttio/dominicanappbackend/DominicanAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.verduttio.dominicanappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominicanAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DominicanAppBackendApplication.class, args);
+    }
+
+}
---
+++ backend/dominicanAppBackend/src/test/java/org/verduttio/dominicanappbackend/DominicanAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.verduttio.dominicanappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominicanAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
