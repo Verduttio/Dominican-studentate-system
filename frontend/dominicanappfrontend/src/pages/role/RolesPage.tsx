c1fd3fac84bfc8df8508ac5d7dcb6d348e618c2b
Verduttio <bszwaja20@gmail.com>
2024-02-21 11:18:04 +0100

    feat(frontend): delete button for each role on role page

--- frontend/dominicanappfrontend/src/pages/role/RolesPage.tsx
+++ frontend/dominicanappfrontend/src/pages/role/RolesPage.tsx
@@ -7,11 +7,18 @@ import {useNavigate} from "react-router-dom";
 function ViewRoles() {
     const [roles, setRoles] = useState<Role[]>([]);
     const { request, error, loading } = useHttp(`${backendUrl}/api/roles`, 'GET');
+    const {error: deleteRoleError, loading: deleteRoleLoading, request: deleteRoleRequest} = useHttp();
     const navigate = useNavigate();
 
     useEffect(() => {
         request(null, (data: Role[]) => setRoles(data));
     }, [request]);
+
+    const handleDelete = (id: number) => {
+        deleteRoleRequest(null, () => {
+                request(null, (data: Role[]) => setRoles(data));
+        }, false, `${backendUrl}/api/roles/${id}`, 'DELETE');
+    }
 
     if (loading) return <div>Ładowanie...</div>;
     if (error) return <div className="error-message">{error}</div>;
@@ -19,12 +26,15 @@ function ViewRoles() {
     return (
         <div className="fade-in">
             <h1>Role</h1>
+            {deleteRoleError && <div className="error-message">{deleteRoleError}</div>}
             <table>
                 <thead>
                 <tr>
                     <th>ID</th>
                     <th>Nazwa</th>
                     <th>Typ</th>
+                    <th>Edytuj</th>
+                    <th>Usuń</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -33,6 +43,8 @@ function ViewRoles() {
                         <td>{role.id}</td>
                         <td>{role.name}</td>
                         <td>{role.type}</td>
+                        <td><button className="btn btn-warning" onClick={() => navigate(`/edit-role/${role.id}`)}>Edytuj</button></td>
+                        <td><button className="btn btn-danger" onClick={() => {handleDelete(role.id)}} disabled={deleteRoleLoading}>Usuń</button></td>
                     </tr>
                 ))}
                 </tbody>
