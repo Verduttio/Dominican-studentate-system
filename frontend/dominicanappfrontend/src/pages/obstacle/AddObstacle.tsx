2c0added3db5b07be33de576897aadf30087a674
Verduttio <bszwaja20@gmail.com>
2024-02-22 12:05:43 +0100

    feat(frontend): improve add obstacle UI

--- frontend/dominicanappfrontend/src/pages/obstacle/AddObstacle.tsx
+++ frontend/dominicanappfrontend/src/pages/obstacle/AddObstacle.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import useHttp from "../../services/UseHttp";
 import {ObstacleData, TaskShortInfo, UserShortInfo} from "../../models/interfaces";
 import {backendUrl} from "../../utils/constants";
+import LoadingSpinner from "../../components/LoadingScreen";
 
 function AddObstacle() {
     const initialObstacleState: ObstacleData = {
@@ -15,7 +16,7 @@ function AddObstacle() {
 
     const [obstacleData, setObstacleData] = useState<ObstacleData>(initialObstacleState);
     const [validationError, setValidationError] = useState<string>('');
-    const { request: postObstacle, error: postError } = useHttp(`${backendUrl}/api/obstacles`, 'POST');
+    const { request: postObstacle, error: postError, loading: postLoading } = useHttp(`${backendUrl}/api/obstacles`, 'POST');
     const [users, setUsers] = useState<UserShortInfo[]>([]);
     const { request: fetchUsers, error: fetchUsersError, loading: loadingFetchUser} = useHttp(`${backendUrl}/api/users/shortInfo`, 'GET');
     const [tasks, setTasks] = useState<TaskShortInfo[]>([]);
@@ -30,13 +31,13 @@ function AddObstacle() {
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
-        if (!obstacleData.userId || !obstacleData.taskId || !obstacleData.fromDate || !obstacleData.toDate || !obstacleData.applicantDescription) {
-            setValidationError('Wszystkie pola muszą być wypełnione.');
+        if (!obstacleData.userId || !obstacleData.taskId || !obstacleData.fromDate || !obstacleData.toDate) {
+            setValidationError('Wypełnij wymagane pola');
             return;
         }
 
         postObstacle(obstacleData, () => {
-            navigate('/obstacles');
+            navigate('/obstacles', {state: {message: 'Pomyślnie dodano przeszkodę'}});
         });
     };
 
@@ -55,68 +56,88 @@ function AddObstacle() {
         setObstacleData({ ...obstacleData, taskId: parseInt(e.target.value) });
     }
 
-    if(fetchUsersError) return <div className="error-message">{fetchUsersError}</div>;
-    if(fetchTasksError) return <div className="error-message">{fetchTasksError}</div>;
-    if(loadingFetchUser || loadingFetchTasks) return <div>Ładowanie...</div>;
+    if(loadingFetchUser || loadingFetchTasks) return <LoadingSpinner/>;
+    if(fetchUsersError) return <div className="alert alert-danger">{fetchUsersError}</div>;
+    if(fetchTasksError) return <div className="alert alert-danger">{fetchTasksError}</div>;
 
     return (
         <div className="fade-in">
-            {postError && <div className="error-message">{postError}</div>}
-            {validationError && <div className="error-message">{validationError}</div>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="userId">Użytkownik:</label>
-                    <select id="userId" value={obstacleData.userId} onChange={handleUserChange}>
-                        <option value="">Wybierz użytkownika</option>
-                        {users.map(user => (
-                            <option key={user.id} value={user.id}>
-                                {user.name} {user.surname}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="taskId">Zadanie:</label>
-                    <select id="taskId" value={obstacleData.taskId} onChange={handleTaskChange}>
-                        <option value="">Wybierz zadanie</option>
-                        {tasks.map(task => (
-                            <option key={task.id} value={task.id}>
-                                {task.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="fromDate">Data Początkowa:</label>
-                    <input
-                        type="date"
-                        id="fromDate"
-                        name="fromDate"
-                        value={obstacleData.fromDate}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="toDate">Data Końcowa:</label>
-                    <input
-                        type="date"
-                        id="toDate"
-                        name="toDate"
-                        value={obstacleData.toDate}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="applicantDescription">Opis Wniosku:</label>
-                    <textarea
-                        id="applicantDescription"
-                        name="applicantDescription"
-                        value={obstacleData.applicantDescription}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">Dodaj Przeszkodę</button>
-            </form>
+            <div className="page-header">
+                <h1>Dodaj przeszkodę</h1>
+            </div>
+            <div className="edit-entity-container">
+                {postError && <div className="alert alert-danger">{postError}</div>}
+                {validationError && <div className="alert alert-danger">{validationError}</div>}
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="userId" className="form-label">Użytkownik:</label>
+                        <select
+                            className="form-select"
+                            id="userId"
+                            value={obstacleData.userId}
+                            onChange={handleUserChange}
+                        >
+                            <option value="">Wybierz użytkownika</option>
+                            {users.map(user => (
+                                <option key={user.id} value={user.id}>
+                                    {user.name} {user.surname}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="taskId" className="form-label">Zadanie:</label>
+                        <select
+                            className="form-select"
+                            id="taskId"
+                            value={obstacleData.taskId}
+                            onChange={handleTaskChange}
+                        >
+                            <option value="">Wybierz zadanie</option>
+                            {tasks.map(task => (
+                                <option key={task.id} value={task.id}>
+                                    {task.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="fromDate" className="form-label">Data początkowa:</label>
+                        <input
+                            className="form-control"
+                            type="date"
+                            id="fromDate"
+                            name="fromDate"
+                            value={obstacleData.fromDate}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="toDate" className="form-label">Data końcowa:</label>
+                        <input
+                            className="form-control"
+                            type="date"
+                            id="toDate"
+                            name="toDate"
+                            value={obstacleData.toDate}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="applicantDescription" className="form-label">Opis wniosku (opcjonalnie):</label>
+                        <textarea
+                            className="form-control"
+                            id="applicantDescription"
+                            name="applicantDescription"
+                            value={obstacleData.applicantDescription}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <button className="btn btn-success" type="submit" disabled={postLoading}>Dodaj</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
