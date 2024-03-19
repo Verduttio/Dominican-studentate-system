882ff13a5a140c7db600bc00130e2a71266fe453
Verduttio <bszwaja20@gmail.com>
2024-03-19 13:13:47 +0100

    fix(frontend): change constants backend frontend url to be specified by env not dynamically through window.location

--- frontend/dominicanappfrontend/src/utils/constants.ts
+++ frontend/dominicanappfrontend/src/utils/constants.ts
@@ -1,13 +1,2 @@
-const getApiBaseUrl = (port: number) => {
-    if(window.location.port === '80') {
-        return `${window.location.protocol}//${window.location.hostname}`;
-    } else {
-        return `${window.location.protocol}//${window.location.hostname}:${port}`;
-    }
-}
-
-// export const backendUrl : string  = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
-// export const frontendUrl : string  = process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
-
-export const backendUrl : string  = getApiBaseUrl(8080) || "http://localhost:8080";
-export const frontendUrl : string  = getApiBaseUrl(3000) || "http://localhost:3000";
+export const backendUrl : string  = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+export const frontendUrl : string  = process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
