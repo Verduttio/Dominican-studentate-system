5d9fbbd82d6a748a7607f96de78ca3e884cf4737
Verduttio <bszwaja20@gmail.com>
2024-03-24 23:40:46 +0100

    feat(backend): update sequences after insertions through migration

---
+++ backend/dominicanAppBackend/src/main/resources/db/migration/V3__Update_sequences_after_insertions.sql
@@ -0,0 +1,6 @@
+SELECT setval('role_id_seq', (SELECT MAX(id) FROM roles));
+SELECT setval('conflict_id_seq', (SELECT MAX(id) FROM conflicts));
+SELECT setval('obstacle_id_seq', (SELECT MAX(id) FROM obstacles));
+SELECT setval('schedule_id_seq', (SELECT MAX(id) FROM schedule));
+SELECT setval('task_id_seq', (SELECT MAX(id) FROM tasks));
+SELECT setval('user_id_seq', (SELECT MAX(id) FROM users));
