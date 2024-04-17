8047445f7d35cae0a44129a7c14058f9d90fb283
Verduttio <bszwaja20@gmail.com>
2024-04-17 17:30:33 +0200

    feat(backend): clean up pdf service and use more space on pdf page

--- backend/dominicanAppBackend/src/main/java/org/verduttio/dominicanappbackend/service/PdfService.java
+++ backend/dominicanAppBackend/src/main/java/org/verduttio/dominicanappbackend/service/PdfService.java
@@ -27,7 +27,7 @@ import java.util.List;
 public class PdfService {
 
     private final ScheduleService scheduleService;
-    private static final float MARGIN = 30;
+    private static final float MARGIN = 20;
     private static final String FONT_PATH = "Capsuula.ttf";
 
     @Autowired
@@ -96,14 +96,14 @@ public class PdfService {
     }
 
     private float initializeTitle(PDDocument doc, PDPage page, PDFont font, LocalDate from, LocalDate to) throws IOException {
-        return addTitle(doc, page, font, "Harmonogram od " + from + " do " + to);
+        return addTitle(doc, page, font, "Oficja od " + from + " do " + to);
     }
 
     private static float addTitle(PDDocument doc, PDPage page, PDFont font, String title) throws IOException {
-        float titleWidth = font.getStringWidth(title) / 1000 * 18; // Font size 18
+        float titleWidth = font.getStringWidth(title) / 1000 *18; // Font size 18
         float titleHeight = font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * 18;
-        float startX = (page.getMediaBox().getWidth() - titleWidth) / 2;
-        float startY = page.getMediaBox().getHeight() - 2 * MARGIN - 20 - titleHeight;
+        float startX = (page.getMediaBox().getWidth()) / 2 - titleWidth / 2;
+        float startY = page.getMediaBox().getHeight() - MARGIN - titleHeight;
 
         try (PDPageContentStream contentStream = new PDPageContentStream(doc, page)) {
             contentStream.beginText();
@@ -112,11 +112,11 @@ public class PdfService {
             contentStream.showText(title);
             contentStream.endText();
         }
-        return startY - 30 - titleHeight;
+        return startY - titleHeight;
     }
 
     private BaseTable initializeTable(PDDocument doc, PDPage page, float startY) throws IOException {
-        return new BaseTable(startY, startY, MARGIN, page.getMediaBox().getWidth() - 2 * MARGIN, MARGIN, doc, page, true, true);
+        return new BaseTable(startY, page.getMediaBox().getHeight() - MARGIN, MARGIN, page.getMediaBox().getWidth() - 2 * MARGIN, MARGIN, doc, page, true, true);
     }
 
     private void populateUserScheduleTable(BaseTable table, List<ScheduleShortInfoForUser> schedules, PDFont font) throws IOException {
@@ -172,72 +172,91 @@ public class PdfService {
 
         table.addHeaderRow(headerRow);
 
-        boolean firstTask = true;
-        // Iterate over schedules and add rows for each task and user
+
         for (ScheduleShortInfoForTask schedule : schedules) {
             String taskName = schedule.taskName();
-
-            if (!firstTask) {
-                // Add empty row as separator
-                Row<PDPage> separatorRow = table.createRow(5f);
-                Cell<PDPage> separatorCell = separatorRow.createCell(100, "");
-                separatorCell.setFillColor(Color.DARK_GRAY);
-            } else {
-                firstTask = false;
-            }
-
-            // Cell for task name
-            int cellForTaskNameNumber = schedule.usersInfoStrings().size() / 2;
-            if (schedule.usersInfoStrings().size() % 2 == 0) {
-                cellForTaskNameNumber -= 1;
-            }
+            int cellNumberForTaskName = getCellNumberForTaskName(schedule);
 
             if (schedule.usersInfoStrings().isEmpty()) {
-                cellForTaskNameNumber = 0;
-            }
-
-            Row<PDPage> taskRow = table.createRow(12f);
-            String text = "";
-            if(cellForTaskNameNumber == 0) {
-                text = taskName;
-            }
-            Cell<PDPage> taskCell = taskRow.createCell(50, text);
-            taskCell.setFillColor(Color.LIGHT_GRAY);
-            taskCell.setAlign(HorizontalAlignment.CENTER);
-            taskCell.setFont(font);
-            taskCell.setFontSize(12);
-            taskCell.setTopBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
-            taskCell.setBottomBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
-
-            //Add one row for each user
-            for (int i = 0; i < schedule.usersInfoStrings().size(); i++) {
-                Cell<PDPage> userCell = taskRow.createCell(50, schedule.usersInfoStrings().get(i));
-                userCell.setFont(font);
-                userCell.setFontSize(12);
-
-                if (i+1 < schedule.usersInfoStrings().size()) {
-                    Row<PDPage> newUserRow = table.createRow(12f);
-                    text = "";
-                    if (i+1 == cellForTaskNameNumber) {
-                        text = taskName;
-                    }
-                    Cell<PDPage> emptyTaskCell = newUserRow.createCell(50, text);
-                    emptyTaskCell.setFont(font);
-                    emptyTaskCell.setFontSize(12);
-                    emptyTaskCell.setFillColor(Color.LIGHT_GRAY);
-                    emptyTaskCell.setAlign(HorizontalAlignment.CENTER);
-                    emptyTaskCell.setTopBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
-                    if(i+1 == schedule.usersInfoStrings().size() - 1) {
-                        emptyTaskCell.setBottomBorderStyle(new LineStyle(Color.BLACK, 1));
+                createRowWithTaskNameButWithoutUserName(table, taskName, font);
+            } else {
+                for (int i = 0; i < schedule.usersInfoStrings().size(); i++) {
+                    if (i == cellNumberForTaskName) {
+                        createRowWithTaskNameAndUserName(table, taskName, font, schedule.usersInfoStrings().get(i));
                     } else {
-                        emptyTaskCell.setBottomBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
+                        createRowWithNoTaskNameButWithUserName(table, schedule.usersInfoStrings().get(i), font);
                     }
-                    taskRow = newUserRow;
                 }
             }
+
+            createSeparatorBetweenTasks(table);
         }
 
         table.draw();
+    }
+
+    private static void createRowWithTaskNameAndUserName(BaseTable table, String taskName, PDFont font, String userName) {
+        Row<PDPage> taskRow = table.createRow(12f);
+        Cell<PDPage> taskCell = taskRow.createCell(50, taskName);
+        taskCell.setFillColor(Color.LIGHT_GRAY);
+        taskCell.setAlign(HorizontalAlignment.CENTER);
+        taskCell.setFont(font);
+        taskCell.setFontSize(12);
+        taskCell.setTopBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
+        taskCell.setBottomBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
+
+        Cell<PDPage> userCell = taskRow.createCell(50, userName);
+        userCell.setFont(font);
+        userCell.setFontSize(12);
+    }
+
+    private static void createRowWithTaskNameButWithoutUserName(BaseTable table, String taskName, PDFont font) {
+        Row<PDPage> taskRow = table.createRow(12f);
+        Cell<PDPage> taskCell = taskRow.createCell(50, taskName);
+        taskCell.setFillColor(Color.LIGHT_GRAY);
+        taskCell.setAlign(HorizontalAlignment.CENTER);
+        taskCell.setFont(font);
+        taskCell.setFontSize(12);
+        taskCell.setTopBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
+        taskCell.setBottomBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
+
+        Cell<PDPage> userCell = taskRow.createCell(50, "");
+        userCell.setFillColor(Color.LIGHT_GRAY);
+        userCell.setBottomBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
+    }
+
+    private static void createRowWithNoTaskNameButWithUserName(BaseTable table, String userName, PDFont font) {
+        // Empty cell
+        Row<PDPage> newUserRow = table.createRow(12f);
+        Cell<PDPage> emptyTaskCell = newUserRow.createCell(50, "");
+        emptyTaskCell.setFont(font);
+        emptyTaskCell.setFontSize(12);
+        emptyTaskCell.setFillColor(Color.LIGHT_GRAY);
+        emptyTaskCell.setAlign(HorizontalAlignment.CENTER);
+        emptyTaskCell.setTopBorderStyle(new LineStyle(Color.LIGHT_GRAY, 0));
+
+        // User cell
+        Cell<PDPage> userCell = newUserRow.createCell(50, userName);
+        userCell.setFont(font);
+        userCell.setFontSize(12);
+    }
+
+    private static void createSeparatorBetweenTasks(BaseTable table) {
+        Row<PDPage> separatorRow = table.createRow(5f);
+        Cell<PDPage> separatorCell = separatorRow.createCell(100, "");
+        separatorCell.setFillColor(Color.DARK_GRAY);
+    }
+
+    private static int getCellNumberForTaskName(ScheduleShortInfoForTask schedule) {
+        int cellForTaskNameNumber = schedule.usersInfoStrings().size() / 2;
+        if (schedule.usersInfoStrings().size() % 2 == 0) {
+            cellForTaskNameNumber -= 1;
+        }
+
+        if (schedule.usersInfoStrings().isEmpty()) {
+            cellForTaskNameNumber = 0;
+        }
+        return cellForTaskNameNumber;
     }
 
     private byte[] finalizeDocument(PDDocument doc) throws IOException {
