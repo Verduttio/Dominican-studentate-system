6bc4f5e2f8b53f39d55025de5ebc3446922824c7
Verduttio <bszwaja20@gmail.com>
2024-10-17 17:00:46 +0200

    feat(frontend): add info about link structure in LinkFormFields

--- frontend/dominicanappfrontend/src/pages/links/LinkFormFields.tsx
+++ frontend/dominicanappfrontend/src/pages/links/LinkFormFields.tsx
@@ -4,6 +4,8 @@ import useHttp from "../../services/UseHttp";
 import {backendUrl} from "../../utils/constants";
 import LoadingSpinner from "../../components/LoadingScreen";
 import AlertBox from "../../components/AlertBox";
+import {faCircleInfo} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 interface LinkFormFieldsProps {
     documentLink: DocumentLink | null;
@@ -13,6 +15,7 @@ interface LinkFormFieldsProps {
 const LinkFormFields: React.FC<LinkFormFieldsProps> = ({ documentLink, setDocumentLink }) => {
     const [documentLinks, setDocumentLinks] = useState<DocumentLink[]>([]);
     const { request: getDocumentLinks, error: errorGetDocumentLinks, loading: loadingGetDocumentLinks } = useHttp(`${backendUrl}/api/document-links`, 'GET');
+    const [showPopup, setShowPopup] = useState(false);
 
     useEffect(() => {
         getDocumentLinks(null, (data: DocumentLink[]) => {
@@ -34,6 +37,20 @@ const LinkFormFields: React.FC<LinkFormFieldsProps> = ({ documentLink, setDocume
         });
     };
 
+    const handleIconClick = () => {
+        setShowPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+
+    const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget) {
+            setShowPopup(false);
+        }
+    };
+
     if (loadingGetDocumentLinks) return <LoadingSpinner/>;
     if (errorGetDocumentLinks) return <AlertBox text={errorGetDocumentLinks} type={"danger"} width={"500px"}/>;
 
@@ -52,7 +69,15 @@ const LinkFormFields: React.FC<LinkFormFieldsProps> = ({ documentLink, setDocume
                 />
             </div>
             <div className="mb-3">
-                <label htmlFor="linkUrl" className="form-label">URL:</label>
+                <label htmlFor="linkUrl" className="form-label">URL:{" "}
+                    <span
+                    style={{cursor: "pointer"}}
+                    onClick={handleIconClick}
+                    title="Kliknij po więcej informacji"
+                >
+                        <FontAwesomeIcon icon={faCircleInfo}/>
+                    </span>
+                </label>
                 <input
                     id="linkUrl"
                     name="url"
@@ -79,6 +104,45 @@ const LinkFormFields: React.FC<LinkFormFieldsProps> = ({ documentLink, setDocume
                     <option key={documentLinks.length+1} value={documentLinks.length+1}>{'{--Wstaw na koniec--}'}</option>
                 </select>
             </div>
+            {showPopup && (
+                <div
+                    className="custom-modal-backdrop fade-in"
+                    onClick={handleBackdropClick}
+                >
+                    <div className="card custom-modal">
+                        <div className="card-body">
+                            <h5 className="card-title">Informacja o linku</h5>
+                            <p className="card-text">
+                                Upewnij się, że wpisujesz pełny adres URL zaczynający się od{" "}
+                                <code>https://</code>.
+                                <br />Przykład:{" "}
+                                <code>https://research.google.com/pubs/archive/44678.pdf</code>
+                            </p>
+                            <p className="card-text">
+                                W przypadku plików udostępnianych z Google Drive,
+                                należy upewnić się, że plik jest widoczny publicznie, lub że osoba aktualnie zalogowana
+                                ma do niego dostęp.<br/>
+                                Link można skopiować bezpośrednio z przeglądarki po otwarciu pliku.
+                                Lub użyć opcji "Udostępnij" i skopiować link z tamtej sekcji.<br/>
+                                W przypadku plików pdf, link powinien kończyć się na <code>.pdf</code> lub <code>.../preview</code>.<br />
+                                Ważne jest to szczególnie w przypadku plików pdf z Google Drive,
+                                ponieważ wtedy należy skopiować link i zamienić <code>/edit</code> na <code>/preview</code>.<br/>
+                                Przykład poprawnego formatu linku: <code>https://drive.google.com/file/d/1t5/<b>preview</b></code><br/>
+                                <br/>
+                                Pozostałe pliki jak word, excel powinny działać normalnie poprzez skopiowanie linku z przeglądarki.
+                            </p>
+                            <div className="text-center">
+                                <button
+                                    className="btn btn-secondary"
+                                    onClick={handleClosePopup}
+                                >
+                                    Zamknij
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
