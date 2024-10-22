93337f53e41ee3b1cf3d918772accd74ea066994
Verduttio <bszwaja20@gmail.com>
2024-10-22 12:22:13 +0200

    refactor(frontend): change default visibility in obstacle form to false

--- frontend/dominicanappfrontend/src/pages/task/addTask/AddTask.tsx
+++ frontend/dominicanappfrontend/src/pages/task/addTask/AddTask.tsx
@@ -48,7 +48,7 @@ function AddTask() {
         supervisorRoleName: '',
         daysOfWeek: [],
         sortOrder: 0,
-        visibleInObstacleFormForUserRole: true,
+        visibleInObstacleFormForUserRole: false,
     };
 
     const [taskData, setTaskData] = useState(initialTaskState);
