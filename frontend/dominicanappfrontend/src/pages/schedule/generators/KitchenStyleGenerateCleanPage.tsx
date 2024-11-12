6508e885b5b90b72a6995d246e080775e43640d1
Verduttio <bszwaja20@gmail.com>
2024-11-12 19:14:51 +0100

    feat(frontend): add role authorization in KitchenStyleGenerateCleanPage.tsx

--- frontend/dominicanappfrontend/src/pages/schedule/generators/KitchenStyleGenerateCleanPage.tsx
+++ frontend/dominicanappfrontend/src/pages/schedule/generators/KitchenStyleGenerateCleanPage.tsx
@@ -1,9 +1,17 @@
 import KitchenStyleGenerator from "./KitchenStyleGenerator";
 import KitchenStyleCleaner from "./KitchenStyleCleaner";
+import useIsFunkcyjny, {UNAUTHORIZED_PAGE_TEXT} from "../../../services/UseIsFunkcyjny";
+import LoadingSpinner from "../../../components/LoadingScreen";
+import AlertBox from "../../../components/AlertBox";
+import React from "react";
 
 
 function KitchenStyleGenerateCleanPage() {
+    const {isFunkcyjny, isFunkcyjnyLoading, isFunkcyjnyError} = useIsFunkcyjny();
 
+    if (isFunkcyjnyLoading) return <LoadingSpinner/>
+    if (isFunkcyjnyError) return <AlertBox text={isFunkcyjnyError} type={"danger"} width={"500px"}/>
+    if (!isFunkcyjny) return <AlertBox text={UNAUTHORIZED_PAGE_TEXT} type="danger" width={'500px'} />
 
     return (
         <div className="fade-in">
