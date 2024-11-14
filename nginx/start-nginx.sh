c690500caed3f300402cf0691e7c7a4136fb39f9
Bartłomiej Szwaja <72033031+Verduttio@users.noreply.github.com>
2024-11-14 13:51:00 +0100

    feat: automated cert renewal without stopping the app (#3)
    * build: add certbot to docker compose
    
    * build: edit nginx configuration to support certbot operations
    
    * build: add certbot certification paths to docker-compose.yml
    
    * build: configure nginx to use certifications generated by certbot
    
    * build: add variable to nginx configuration and fetch it from .env file
    
    * build: update deploy_new_version.sh to give privileges to run scripts
    
    * build: fix typo in start-nginx.sh
    
    * build: add SERVER_NAME variable in docker-compose.yml and fix paths do nginx template
    
    * build: remove default.conf because we have now default.conf.template

---
+++ nginx/start-nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Remove 'http://' from the beginning of SERVER_NAME
+SERVER_NAME_CLEAN=$(echo "$SERVER_NAME" | sed 's|^https\?://||')
+
+# Export variable to `envsubst`
+export SERVER_NAME_CLEAN
+
+# Use `envsubst`, to replace variable in template and save output as nginx configuration file
+envsubst '${SERVER_NAME_CLEAN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Run Nginx
+exec nginx -g 'daemon off;'
