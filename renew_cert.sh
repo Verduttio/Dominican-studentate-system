90c86f555b0242de94bf963e40285ed0f644b96f
Verduttio <bszwaja20@gmail.com>
2025-05-04 11:24:19 +0200

    feat: provide an absolute path in renew_cert.sh

--- renew_cert.sh
+++ renew_cert.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+echo "$SCRIPT_DIR"
+
 echo "Renewing certificate..."
-RESULT=$(docker compose run --rm certbot renew 2>&1)
+RESULT=$(docker compose -f "$SCRIPT_DIR/docker-compose.yml" run --rm certbot renew 2>&1)
+
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Make sure logs exist
+mkdir -p "$SCRIPT_DIR/logs"
+
+LOGFILE="$SCRIPT_DIR/logs/certbot_renew_$TIMESTAMP.log"
+
+echo "$RESULT" > "$LOGFILE"
 
 if echo "$RESULT" | grep -q "Congratulations, all renewals succeeded"; then
     echo "Certificate renewed successfully."
     echo "Restarting container of nginx..."
-    docker compose restart nginx
+    docker compose -f "$SCRIPT_DIR/docker-compose.yml" restart nginx
 else
     echo "Certificate renewal failed or not needed. Nginx restart skipped."
     echo "Certbot output:"
